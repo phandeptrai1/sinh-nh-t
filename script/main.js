ac4beff38cab558df2c5eabc0489fb954571a775
phandeptrai1 <168959102+phandeptrai1@users.noreply.github.com>
2025-06-15 09:06:32 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Em có muốn mở nhạc nền không?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
